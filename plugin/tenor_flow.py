6461b8119d3e3e96036340c443c56ef592a46642
Garulf <535299+Garulf@users.noreply.github.com>
2021-11-30 04:26:57 -0500

    init

---
+++ plugin/tenor_flow.py
@@ -0,0 +1,12 @@
+from flox import Flox
+
+class TenorFlow(Flox):
+
+    def query(self, query):
+        pass
+
+    def context_menu(self, data):
+        pass
+
+if __name__ == "__main__":
+    TenorFlow()
